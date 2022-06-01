459cd63cc52aff68481f64729ebca01c3127ee3b
Mateus Pereira <mbpereira98@gmail.com>
2022-06-01 20:37:15 -0300

    refactoring

--- src/services/processors/file.js
+++ src/services/processors/file.js
@@ -2,45 +2,45 @@ const fs = require('fs')
 const { readline } = require('../../utils/fs')
 
 const createFileProcessor = ({ input, inputEncoding = 'utf-8' }) => {
-  const _transformers = [], chunks = [], originalChunks = []
-  let _reader, _writer, _onFinish
+  const transformers = [], chunks = [], originalChunks = []
+  let reader, writer, _onFinish
 
-  _reader = readline(input, inputEncoding)
+  reader = readline(input, inputEncoding)
 
   const write = queue => {
     if (queue.length <= 0)
-      return _writer.close()
+      return writer.close()
 
-    const ok = _writer.write(queue.shift())
+    const ok = writer.write(queue.shift())
 
     if (!ok)
-      return _writer.once('drain', () => write(queue))
+      return writer.once('drain', () => write(queue))
 
     write(queue)
   }
 
   const processor = {
     transform: cb => {
-      _transformers.push(cb)
+      transformers.push(cb)
       return processor
     },
     writeTo: ({ output, outputEncoding = 'utf-8' }) => {
-      _writer = fs.createWriteStream(output, outputEncoding)
+      writer = fs.createWriteStream(output, outputEncoding)
       return processor
     },
     process: () => {
-      _reader
+      reader
         .onLine(line => {
           originalChunks.push(line)
-          _transformers.forEach(transform => line = transform(line))
+          transformers.forEach(transform => line = transform(line))
           chunks.push(line)
         })
 
-      _reader.onClose(() => {
-        if (!_writer) return _onFinish()
+      reader.onClose(() => {
+        if (!writer) return _onFinish()
 
         write(chunks.slice())
-        _writer.on('close', _onFinish)
+        writer.on('close', _onFinish)
       })
 
       return processor
