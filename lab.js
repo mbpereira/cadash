49b49adee60ed9ee754eed3091df93274d2eca99
Mateus Pereira <mbpereira98@gmail.com>
2022-06-01 04:40:07 -0300

    realizando testes com processamento de arquivos e streams

--- lab.js
+++ lab.js
@@ -1,8 +1,50 @@
 const fs = require('fs')
-const path = require('path')
-const readline = require('readline')
-const stream = fs.createReadStream(path.join('.', 'src', 'controllers', 'upload.js'), { encoding: 'utf8' })
+const { join } = require('path')
+const { readline, getFileExtension } = require('./src/utils/fs')
 
-let i = 0
-const process = readline.createInterface(stream)
-process.on('line', line => console.log(i++, line) && stream.close())
+let lineNumber = 0
+
+const process = filepath => new Promise((res, rej) => {
+	const ext = getFileExtension(filepath)
+	const reader = readline(filepath)
+	const tmpfile = `${filepath}.tmp${ext}`
+	const writer = fs.createWriteStream(tmpfile)
+
+	const queue = []
+
+	reader.on('line', line => {
+		try {
+			line = line + '\n'
+			const matches = line.match(/"(.*?)"/ig)
+			if (Array.isArray(matches))
+				matches.forEach(m => line = line.replace(m, m.replace(/,/ig, ' -')))
+			queue.unshift(line)
+		} catch (e) {
+			rej(e)
+		}
+	})
+
+	reader.on('close', () => {
+		console.log('finish...')
+		save()
+	})
+
+	const save = () => {
+		if (queue.length <= 0) {
+			writer.close()
+			return
+		}
+
+		const ok = writer.write(queue.pop())
+		if (!ok) {
+			writer.once('drain', save)
+			return
+		}
+
+		save()
+	}
+})
+
+process(join('./', 'files', 'survey_results_public.csv'))
+	.then(() => "Processado")
+	.catch(console.error)
